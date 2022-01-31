272ecc861f5a9f89741424a575f709c6ef71b2db
Kpatain <62848924+Kpatain@users.noreply.github.com>
2022-01-31 10:54:22 +0100

    AD_InitialCommit

---
+++ Assets/SetCameraUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraUI : MonoBehaviour
+{
+    Camera camUI;
+
+    // Set Camera UI
+    void Start()
+    {
+        camUI = GameObject.Find("Camera_UI").GetComponent<Camera>();
+        GetComponent<Canvas>().worldCamera = camUI;
+    }
+}
